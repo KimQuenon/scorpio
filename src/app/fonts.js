59f10679106d018f5306bfc569948422dfa4b840
Kim <kimberley.quenon.@gmail.com>
2024-01-24 11:57:30 +0100

    homepage design v2

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,8 +1,17 @@
 import { Inria_Sans } from 'next/font/google'
+import { Playfair_Display } from 'next/font/google'
+
 
 export const inria = Inria_Sans(
     {
         subsets: ['latin'],
         weight: '300',
+    }
+)
+
+export const playfair = Playfair_Display(
+    {
+        subsets: ['latin'],
+        weight: '700',
     }
 )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
-
+import { playfair } from "./fonts"
+import Link from "next/link";
 
 export default function HomePage() {
     return (
@@ -8,7 +9,16 @@ export default function HomePage() {
                 <div className="home_content">
                     <div className="home_title"></div>
                     <div className="home_text">
-                        <div className="home_intro"></div>
+                        <div className="home_intro">
+                            <h1 className={playfair.className}>&quot;In the dance of light and precious metals,</h1>
+                            <h1 className={playfair.className}>find the rhythm of sophistication.&quot;</h1>
+                            <hr></hr>
+                            <p>Step into a world of timeless elegance at our boutique. Discover handcrafted treasures that redefine sophistication and elevate your style with every piece.</p>
+                        </div>
+                        <div className="home_btn">
+                            <button className="btn white"><Link href="/store">BROWSE OUR SHOP &gt;</Link></button>
+                            <button className="btn transparent"><Link href="/about">ABOUT US &gt;</Link></button>
+                        </div>
                     </div>
                 </div>
                 <div className="social_media"></div>
