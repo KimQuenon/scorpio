05dc7e8e3bd9b120b6896b493d1fd40ea6943fc8
Kim <kimberley.quenon.@gmail.com>
2024-01-25 21:54:00 +0100

    store temporaire

--- src/app/store/page.jsx
+++ src/app/store/page.jsx
@@ -2,6 +2,7 @@ import { getJewels } from '@/lib/jewel'
 import Image from 'next/image'
 import Link from 'next/link'
 import Cards from "@/components/Cards";
+import { playfair } from "@/src/app/fonts";
 
 
 export const metadata = {
@@ -13,11 +14,17 @@ export default async function StorePage()
     const jewels = await getJewels()
     return(
         <>
-            <h1>Store</h1>
-            <p>Here we&apos;ll list all the reviews</p>
-            <div className='container'>
-
-                <Cards jewels={jewels} displayCount={5} />
+            <div className="slide" id="latest">
+                <div className="container">
+                    <div className="home_text">
+                        <div className="home_intro">
+                            <h2 className={playfair.className}>SHOP ONLINE</h2>
+                            <hr/>
+                            <p className="text-brown">Discover the epitome of elegance with our newest additions to the collection. Explore the latest trends and timeless classics that have just arrived at our online jewelry store. From captivating designs to exquisite craftsmanship, each piece tells a unique story. </p>
+                        </div>
+                    </div>
+                    <Cards jewels={jewels} displayCount={5} />
+                </div>
             </div>
         </>
     )
